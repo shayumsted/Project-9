ef64d4e32753ca7cafd38f78e3b811fe530330c7
Shamira Umstead <sumstead@usf.edu>
2024-06-09 12:03:06 -0400

    git commit -m "Implement interactive behaviors with D3.js."

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// U78784426
+// Write a function that will initiate the page
+function initializePage() {const dataContainer = d3.select("#data-container");
+
+// Sample data points
+    const data = [10, 20, 30, 40, 50];
+
+// Create the div elements as data points
+    const dataPoints = dataContainer.selectAll(".data-point")
+        .data(data)
+        .enter()
+        .append('div')
+        .classed("data-point", true)
+        .text(d => d);
 
